b4788c31f540d89e453d5cda1e9a645abe6081a2
Ming <mingz6@outlook.com>
2023-02-17 00:24:55 -0700

    Update env file

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const client = new Client({ intents: [
 const { Configuration , OpenAIApi } = require ('openai');
 
 const configuration = new Configuration({
-    organization: process.env.local.OPENAI_ORG,
-    apiKey: process.env.local.OPENAI_KEY,
+    organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
